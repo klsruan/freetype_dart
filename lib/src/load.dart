4461ab4adaf2a53061c2ad27462b52733ff33e72
klsruan <rdkleriston@gmail.com>
2025-08-09 10:51:56 -0300

    Update Files

--- lib/src/load.dart
+++ lib/src/load.dart
@@ -4,27 +4,39 @@ import 'dart:io';
 import 'package:path/path.dart' as p;
 import 'generated_bindings.dart';
 
-Future<FreetypeBinding> loadFreeType() async {
+FreetypeBinding loadFreeType() {
+  if (Platform.isAndroid) {
+    return FreetypeBinding(DynamicLibrary.open('libfreetype.so'));
+  }
+
   final packageConfigUri = Uri.parse(Platform.packageConfig!);
   final packageConfigFile = File.fromUri(packageConfigUri);
+
   if (!packageConfigFile.existsSync()) {
     throw Exception('package_config.json não encontrado em ${packageConfigFile.path}');
   }
-  final content = await packageConfigFile.readAsString();
+
+  final content = packageConfigFile.readAsStringSync();
   final Map<String, dynamic> config = jsonDecode(content);
+
   final packages = config['packages'] as List<dynamic>;
   final freetypePackage = packages.firstWhere(
     (pkg) => pkg['name'] == 'freetype_dart',
     orElse: () => null,
   );
+
   if (freetypePackage == null) {
     throw Exception('Pacote freetype_dart não encontrado no package_config.json');
   }
+
   final rootUriString = freetypePackage['rootUri'] as String;
   final rootUri = Uri.parse(rootUriString);
+
   Uri baseUri = packageConfigUri.resolve('.');
   final packageRootUri = rootUri.isAbsolute ? rootUri : baseUri.resolveUri(rootUri);
+
   final packageRootPath = packageRootUri.toFilePath();
+
   String libName;
   if (Platform.isWindows) {
     libName = 'libfreetype-6.dll';
@@ -35,9 +47,12 @@ Future<FreetypeBinding> loadFreeType() async {
   } else {
     throw UnsupportedError('Unsupported platform');
   }
+
   final libPath = p.join(packageRootPath, 'bin', libName);
+
   if (!File(libPath).existsSync()) {
     throw Exception('Library not found at path: $libPath');
   }
+
   return FreetypeBinding(DynamicLibrary.open(libPath));
 }
