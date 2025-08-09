14db026651870a126c4e8c09f0ffb6a3da8e3b20
klsruan <rdkleriston@gmail.com>
2025-08-09 11:19:10 -0300

    Update Files

--- lib/src/load.dart
+++ lib/src/load.dart
@@ -1,23 +1,15 @@
 import 'dart:ffi';
 import 'dart:io';
 import 'generated_bindings.dart';
-import 'package:path/path.dart' as p;
 
 FreetypeBinding loadFreeType() {
-  final scriptDir = File.fromUri(Platform.script).parent;
-  final binDir = p.join(scriptDir.path, 'bin');
-  String libName;
+  String path = 'bin/';
   if (Platform.isWindows) {
-    libName = 'libfreetype-6.dll';
+    path += 'libfreetype-6.dll';
   } else if (Platform.isMacOS) {
-    libName = 'libfreetype.6.dylib';
+    path += 'libfreetype.6.dylib';
   } else {
-    libName = 'libfreetype.so.6';
+    path += 'libfreetype.so.6';
   }
-  final libPath = p.join(binDir, libName);
-  if (!File(libPath).existsSync()) {
-    throw Exception('Biblioteca freetype não encontrada em: $libPath');
-  }
-  final dylib = DynamicLibrary.open(libPath);
-  return FreetypeBinding(dylib);
+  return FreetypeBinding(DynamicLibrary.open(path));
 }
